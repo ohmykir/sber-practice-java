e8435eebce61469895ac5a6add4bed0452088f3e
Kirill <ohmykir@yandex.ru>
2024-02-26 10:59:34 +0300

    Выполнено первое задание

--- base-syntax/src/main/java/com/test/FirstHomework.java
+++ base-syntax/src/main/java/com/test/FirstHomework.java
@@ -1,43 +1,43 @@
 package com.test;
 
 public class FirstHomework {
-    public static void method1() {
-        for (int i = 1; i < 100; i++) {
-            if (i%7 == 0) {
-                System.out.print(i + " ");
+    private static void method1() {
+        for (int number = 1; number < 100; number++) {
+            if (number % 7 == 0) {
+                System.out.print(number + " ");
             }
         }
         System.out.println();
     }
-    public static void method2() {
+    private static void method2() {
         int[] array = new int[10];
-        int max = 0;
-        int average = 0;
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random()*100);
-            max = Math.max(max, array[i]);
-            average = (average + array[i])/2;
+        int maxValue = 0;
+        int averageValue = 0;
+        for (int index = 0; index < array.length; index++) {
+            array[index] = (int) (Math.random()*100);
+            maxValue = Math.max(maxValue, array[index]);
+            averageValue = (averageValue + array[index])/2;
         }
-        System.out.println("Max value: " + max + " Average value: " + average);
+        System.out.println("Max value: " + maxValue + " Average value: " + averageValue);
     }
-    public static void method3() {
-        for (int i = 0; i < 100; i++) {
+    private static void method3() {
+        for (int number = 0; number < 100; number++) {
             boolean composite = false;
-            for (int j = 2; j < Math.sqrt(i); j++) {
-                if (i % j == 0) {
+            for (int divider = 2; divider < Math.sqrt(number); divider++) {
+                if (number % divider == 0) {
                     composite = true;
                     break;
                 }
             }
             if (!composite) {
-                System.out.print(i + " ");
+                System.out.print(number + " ");
             }
         }
         System.out.println();
     }
-    public static void method4() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
+    private static void method4() {
+        for (int row = 0; row < 3; row++) {
+            for (int column = 0; column < 5; column++) {
                 System.out.print("* ");
             }
             System.out.println();
